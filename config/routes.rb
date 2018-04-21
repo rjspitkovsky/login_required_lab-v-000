83051dfdc77aeec8d04a055742a7675ce6a841c7
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 20:01:38 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'application#home'
+   get '/login' => 'sessions#new'
+   post '/login' => 'sessions#create'
+   get '/welcome' => 'secrets#welcome'
+   get '/secret' => 'secrets#secret'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
