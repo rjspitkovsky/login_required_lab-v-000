9230807ad8c869a345fa2eda09ccddc7c13bfabb
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 20:19:50 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -8,4 +8,10 @@ class SecretsController < ApplicationController
   def show
   end
 
+  private 
+
+    def require_login 
+      return head(:forbidden) unless session.include?(:name)
+    end 
+
 end
